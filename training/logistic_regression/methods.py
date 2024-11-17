c261aa9c93d68cffdd6f3974537f93106e5a0277
Tioluwani <43251329+t1oluwani@users.noreply.github.com>
2024-11-16 20:00:01 -0700

    + Possibly Needed vars

--- training/logistic_regression/methods.py
+++ training/logistic_regression/methods.py
@@ -1,3 +1,18 @@
+epochs = None
+learning_rate = None
+batch_size = None
+batch_size_train = batch_size * 0.2
+batch_size_validate = batch_size * 0.8
+regularization = None
+optimizer = None
+loss = None
+metrics = None
+decay = None
+momentum = None
+log_interval = None
+early_stopping = None
+random_seed = None
+
 def logistic_regression():
   class LogisticRegression():
     def __init__(self):
