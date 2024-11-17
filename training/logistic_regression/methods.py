9934bd1b2c71ac6a719182cbf8db07a961655267
Tioluwani <43251329+t1oluwani@users.noreply.github.com>
2024-11-16 19:55:30 -0700

    + Logistic Regression template

---
+++ training/logistic_regression/methods.py
@@ -0,0 +1,30 @@
+def logistic_regression():
+  class LogisticRegression():
+    def __init__(self):
+      pass
+    
+    def foward(self, X):
+      pass
+    
+  def train():
+    pass
+  
+  def evaluate():
+    pass
+  
+  pass
+  
+def tune_hyperparameters():
+  class LogisticRegression():
+    def __init__(self):
+      pass
+    
+    def foward(self, X):
+      pass
+    
+  pass
+  
+
+  
+  
+
