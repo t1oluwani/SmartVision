9a0664bd5138e128186f1713d0ad11079791a0de
Tioluwani <43251329+t1oluwani@users.noreply.github.com>
2024-11-16 19:46:07 -0700

    add cnn as option

--- frontend/src/components/HandwrittenNumbers.vue
+++ frontend/src/components/HandwrittenNumbers.vue
@@ -11,7 +11,8 @@
           <label for="model">Choose Model:</label>
           <select v-model="selectedModel" id="model">
             <option value="logistic-regression">Logistic Regression</option>
-            <option value="neural-network">Neural Network</option>
+            <option value="fneural-network">FeedFoward Neural Network (FNN)</option>
+            <option value="cneural-network">Convolutional Neural Network (CNN)</option>
           </select>
         </div>
         <button @click="identify">Identify</button>
