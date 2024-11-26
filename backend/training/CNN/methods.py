a486e2897d28a6e63d4a52cdc274664914949129
Tioluwani <43251329+t1oluwani@users.noreply.github.com>
2024-11-26 02:18:03 -0700

    CNN done but accuracy seems unaturally high

--- backend/training/CNN/methods.py
+++ backend/training/CNN/methods.py
@@ -78,9 +78,8 @@ def CNN(device):
                 output = model(data)
                 pred = output.data.max(1, keepdim=True)[1]
                 correct += pred.eq(target.data.view_as(pred)).sum()
-                loss += F.mse_loss(
-                    output, one_hot(target, num_classes=10).float(), size_average=False
-                ).item()
+                los = F.nll_loss(output, target)
+                loss += los.item()
         loss /= len(data_loader.dataset)
 
         # Print the evaluation results in easily readable format
@@ -109,69 +108,6 @@ def CNN(device):
     # Save the model
     results = dict(model=cnn_model)
     return results
-
-
-# def tune_hyper_parameter(target_metric, device):
-#     start = timeit.default_timer()  # start the timer
-
-#     # Initialize variables
-#     best_params = None
-#     best_metric = 0
-
-#     # Define hyperparameters to search over
-#     learning_rates = np.linspace(1e-04, 1e-03, num=10)
-#     weight_decays = np.linspace(1e-05, 1e-04, num=10)
-#     num_epochs = list(range(5, 10))
-
-#     # Load MNIST dataset
-#     image_dimension = 28 * 28
-#     train_loader, validation_loader, test_loader = load_MNIST()
-
-#     for _ in range(25):
-#         # Randomly sample hyperparameters
-#         lr_val = random.choice(learning_rates)
-#         wd_val = random.choice(weight_decays)
-#         ne_val = random.choice(num_epochs)
-#         print(
-#             f"Training with [learning_rate={lr_val}, weight_decay={wd_val}, epochs={ne_val}]"
-#         )
-
-#         # Define the model and optimizer
-#         logistic_model = LogisticRegressionModel().to(device)
-#         optimizer = optim.Adam(
-#             logistic_model.parameters(), lr=lr_val, weight_decay=wd_val
-#         )
-
-#         # Train model and evaluate validation based on the target metric (accuracy or loss)
-#         if target_metric == "acc":
-#             metric = evaluate_accuracy(logistic_model, validation_loader)
-#             for epoch in range(1, ne_val + 1):
-#                 train_for_param(epoch, optimizer, logistic_model, validation_loader)
-#                 metric = evaluate_accuracy(logistic_model, validation_loader)
-#         elif target_metric == "loss":
-#             metric = evaluate_loss(logistic_model, validation_loader)
-#             for epoch in range(1, ne_val + 1):
-#                 train_for_param(epoch, optimizer, logistic_model, validation_loader)
-#                 metric = evaluate_loss(logistic_model, validation_loader)
-#         print(f"Validation {target_metric}: {metric:.4f}")
-
-#         # Check if the current hyperparameters yield a better accuracy
-#         if (target_metric == "acc" and metric > best_metric) or (
-#             target_metric == "loss" and metric < best_metric
-#         ):
-#             best_metric = metric
-#             best_params = {
-#                 "learning_rate": lr_val,
-#                 "weight_decay": wd_val,
-#                 "epochs": ne_val,
-#             }
-
-#     stop = timeit.default_timer()  # stop the timer
-#     run_time = stop - start
-#     print(f"Ran with a runtime of {run_time:.3f} seconds")
-
-#     return best_params, best_metric
-
 
 # HELPER FUNCTIONS
 def load_MNIST():
@@ -214,7 +150,7 @@ def load_MNIST():
         MNIST_validation_set, batch_size=batch_size_train, shuffle=False
     )
     test_loader = torch.utils.data.DataLoader(
-        MNIST_test_set, batch_size=batch_size_test, shuffle=True
+        MNIST_test_set, batch_size=batch_size_test, shuffle=False
     )
 
     return train_loader, validation_loader, test_loader
@@ -236,46 +172,4 @@ def train_for_param(epoch, optimizer, model, data_loader):
         optimizer.step()
 
 
-def evaluate_accuracy(model, data_loader):
-    loss = 0
-    correct = 0
-    dataset = "Validation"
-    one_hot = torch.nn.functional.one_hot
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-    # Evaluation function (accuracy)
-    with torch.no_grad():
-        for data, target in data_loader:
-            data = data.to(device)
-            target = target.to(device)
-            output = model(data)
-            pred = output.data.max(1, keepdim=True)[1]
-            correct += pred.eq(target.data.view_as(pred)).sum()
-            loss += F.mse_loss(
-                output, one_hot(target, num_classes=10).float(), size_average=False
-            ).item()
-    loss /= len(data_loader.dataset)
-    accuracy = 100.0 * correct / len(data_loader.dataset)
-    return accuracy
-
-
-def evaluate_loss(model, data_loader):
-    loss = 0
-    correct = 0
-    dataset = "Validation"
-    one_hot = torch.nn.functional.one_hot
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
-    # Evaluation function (loss)
-    with torch.no_grad():
-        for data, target in data_loader:
-            data = data.to(device)
-            target = target.to(device)
-            output = model(data)
-            pred = output.data.max(1, keepdim=True)[1]
-            correct += pred.eq(target.data.view_as(pred)).sum()
-            loss += F.mse_loss(
-                output, one_hot(target, num_classes=10).float(), size_average=False
-            ).item()
-    loss /= len(data_loader.dataset)
-    return loss
