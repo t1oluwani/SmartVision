6049588836ad55a49849b9d00cac2cbfd671dd5a
Tioluwani <43251329+t1oluwani@users.noreply.github.com>
2024-11-26 19:20:52 -0700

    Add loss function

--- backend/training/ml_models.py
+++ backend/training/ml_models.py
@@ -9,6 +9,7 @@ from torchvision import datasets, transforms
 
 random_seed = 1
 torch.manual_seed(random_seed)
+one_hot = torch.nn.functional.one_hot
 
 batch_size_train = 256
 batch_size_test = 1024
@@ -99,43 +100,7 @@ def FNN(device):
     
     train_loader, validation_loader, test_loader = load_MNIST()
 
-    # Training function
-    def train(data_loader, model, optimizer):
-        model.train()
-
-        for batch_idx, (data, target) in enumerate(data_loader):
-            data = data.to(device)
-            target = target.to(device)
-            optimizer.zero_grad()
-            output = model(data)
-
-            loss = F.cross_entropy(output, target)
-            loss.backward()
-            optimizer.step()
-
-
-    # Evaluation function
-    def evaluate(epoch, data_loader, model, dataset):
-        model.eval()
-        
-        correct = 0
-        eval_loss = 0
-        loading_bar = tqdm(data_loader, ncols=100, position=0, leave=True)
-        
-        with torch.no_grad():
-            for data, target in loading_bar:
-                data = data.to(device)
-                target = target.to(device)
-                output = model(data)
-                pred = output.data.max(1, keepdim=True)[1]
-                correct += pred.eq(target.data.view_as(pred)).sum()
-                loss = F.cross_entropy(output, target)
-                eval_loss += loss.item()
-                loading_bar.set_description("Epoch " + str(epoch) + ": ")
-        eval_loss /= len(data_loader.dataset)
-
-        print(dataset, "Set: | Average Loss: ({:.4f}) | Accuracy Raw: ({}/{}) | Accuracy Percentage: ({:.2f}%) |\n".format(
-                eval_loss, correct, len(data_loader.dataset), 100.0 * correct / len(data_loader.dataset)))
+    
 
     # Train the model
     for epoch in range(1, fnn_model.n_epochs + 1):
@@ -152,8 +117,62 @@ def FNN(device):
 
 # HELPER FUNCTIONS:
 
+# Training function
+def train(data_loader, model, optimizer, loss_type, device):
+    model.train()
+
+    for batch_idx, (data, target) in enumerate(data_loader):
+        data = data.to(device)
+        target = target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+
+        loss = get_loss(loss_type, output, target)
+        loss.backward()
+        optimizer.step()
+
+# Evaluation function
+def evaluate(epoch, data_loader, model, loss_type, dataset, device):
+    model.eval()
+    
+    correct = 0
+    eval_loss = 0
+    loading_bar = tqdm(data_loader, ncols=100, position=0, leave=True)
+    
+    with torch.no_grad():
+        for data, target in loading_bar:
+            data = data.to(device)
+            target = target.to(device)
+            output = model(data)
+            
+            loss = get_loss(loss_type, output, target)
+            eval_loss += loss.item()
+            
+            pred = output.data.max(1, keepdim=True)[1]
+            correct += pred.eq(target.data.view_as(pred)).sum()
+            
+            loading_bar.set_description("Epoch " + str(epoch) + ": ")
+    eval_loss /= len(data_loader.dataset)
+
+    print(dataset, "Set: | Average Loss: ({:.4f}) | Accuracy Raw: ({}/{}) | Accuracy Percentage: ({:.2f}%) |\n".format(
+            eval_loss, correct, len(data_loader.dataset), 100.0 * correct / len(data_loader.dataset)))
+
+
+# Loss function
+def get_loss(loss_type, output, target):
+    if loss_type == "cross_entropy":
+        return F.cross_entropy(output, target)
+    elif loss_type == "mse":
+        return F.mse_loss(output, one_hot(target, num_classes=10).float())
+    elif loss_type == "nll":
+        return F.nll_loss(output, target)
+    else: 
+        print("Invalid loss function")
+        return None
+        
+
+# Load MNIST dataset
 def load_MNIST():
-    # Load MNIST dataset
     MNIST_data_set = datasets.MNIST("/MNIST_dataset/", train=True, download=True,
         transform=transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307), (0.3081))])
     )
