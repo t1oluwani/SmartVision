a4dfa081cb783d15dafc2809c76e1b783faa1975
Tioluwani <43251329+t1oluwani@users.noreply.github.com>
2024-11-26 18:54:27 -0700

    rm num_classes and in_channels

--- backend/training/ml_models.py
+++ backend/training/ml_models.py
@@ -13,17 +13,47 @@ torch.manual_seed(random_seed)
 batch_size_train = 256
 batch_size_test = 1024
 
+# CNN Architecture for MNIST
+class CNNModel(nn.Module):
+    def __init__(self):
+        super(CNNModel, self).__init__()
+        
+        # CNN Hyperparameters
+        self.n_epochs = 10
+        self.w_decay = 1e-05
+        self.learning_rate = 1e-03
+        
+        # CNN Layers
+        self.conv1 = nn.Conv2d(1, 6, kernel_size=5)
+        self.conv2 = nn.Conv2d(6, 16, kernel_size=5)
+        self.fc1 = nn.Linear(256, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.max_pool2d(x, kernel_size=2, stride=2)
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(x, kernel_size=2, stride=2)
+        x = x.view(x.size(0), -1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        output = F.log_softmax(x, dim=1)
+
+        return output
+
 # FNN Architecture for MNIST
 class FNNModel(nn.Module):
-    def __init__(self, num_classes):
+    def __init__(self):
         super(FNNModel, self).__init__()
 
-        # Optimal hyperparameters for FNN model
+        # FNN Hyperparameters
         self.n_epochs = 10
         self.momentum = 0.5
         self.learning_rate = 1e-01
-        self.num_classes = num_classes
         
+        # FNN Layers
         self.fc1 = nn.Linear(784, 64)
         self.fc2 = nn.Linear(64, 32)
         self.fc3 = nn.Linear(32, 10)
@@ -38,16 +68,11 @@ class FNNModel(nn.Module):
 
 
 def FNN(device):
+    
+    fnn_model = FNNModel().to(device)
+    fnn_optimizer = optim.SGD(fnn_model.parameters(), lr=fnn_model.learning_rate, momentum=fnn_model.momentum)
+    
     train_loader, validation_loader, test_loader = load_MNIST()
-    num_classes = 10  # 10 classes for MNIST
-
-    # Define the model and optimizer
-    fnn_model = FNNModel(num_classes).to(device)
-    # optimizer = optim.Adam(
-    #     fnn_model.parameters(), lr=learning_rate, weight_decay=w_decay
-    # )
-    optimizer = optim.SGD(fnn_model.parameters(), lr=fnn_model.learning_rate, momentum=fnn_model.momentum)
-    one_hot = torch.nn.functional.one_hot
 
     # Training function
     def train(data_loader, model, optimizer):
@@ -89,9 +114,9 @@ def FNN(device):
 
     # Train the model
     for epoch in range(1, fnn_model.n_epochs + 1):
-        train(train_loader, fnn_model, optimizer)
+        train(train_loader, fnn_model, fnn_optimizer)
         evaluate(epoch, validation_loader, fnn_model, "Validation")
-
+        
     # Test the model
     evaluate("T", test_loader, fnn_model, "Test")
 
@@ -103,15 +128,17 @@ def FNN(device):
 # HELPER FUNCTIONS:
 
 def load_MNIST():
-    # Load the MNIST dataset for training, validation, and testing
+    # Load MNIST dataset
     MNIST_data_set = datasets.MNIST("/MNIST_dataset/", train=True, download=True,
-        transform=transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307), (0.3081))]))
-    
+        transform=transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307), (0.3081))])
+    )
     MNIST_test_set = datasets.MNIST(
         "/MNIST_dataset/", train=False, download=True,
-        transform=transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307), (0.3081))]))
-
-    MNIST_training_set, MNIST_validation_set = random_split(MNIST_data_set, [48000, 12000])
+        transform=transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307), (0.3081))])
+    )
+    MNIST_training_set, MNIST_validation_set = random_split(
+        MNIST_data_set, [48000, 12000]
+    )
 
     # Data loaders
     train_loader = torch.utils.data.DataLoader(
