1636b5f8592202e3fa9554e72685aa06916fc5d2
Tioluwani <43251329+t1oluwani@users.noreply.github.com>
2024-11-28 17:08:01 -0700

    canvas visible

--- frontend/src/components/subcomponents/DrawingCanvas.vue
+++ frontend/src/components/subcomponents/DrawingCanvas.vue
@@ -1,8 +1,17 @@
 <template>
   <div>
-
+    <canvas id="drawingCanvas" width="500" height="500"></canvas>
   </div>
 </template>
 
 <script>
+
 </script>
+
+<style scoped>
+canvas {
+  border: 2px solid black;
+  background-color: white;
+  /* cursor: url('pen-cursor.png'), auto; Replace 'pen-cursor.png' with your cursor image */
+}
+</style>
