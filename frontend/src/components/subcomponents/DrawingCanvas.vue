e9d94462c929764467651a32629e3f371f2bbd46
Tioluwani <43251329+t1oluwani@users.noreply.github.com>
2024-11-28 22:04:40 -0700

    Attempt to draw on canvas

--- frontend/src/components/subcomponents/DrawingCanvas.vue
+++ frontend/src/components/subcomponents/DrawingCanvas.vue
@@ -14,23 +14,65 @@
 </template>
 
 <script>
-  // Setup Canvas and Graphics Content
-  let cnv = document.getElementById("myCanvas");
-  let ctx = cnv.getContext("2d");
+export default {
+  mounted() {
+    // Setup Canvas and Graphics Context
+    let cnv = document.getElementById("drawingCanvas");
+    if (!cnv) {
+      console.error("Canvas element not found!");
+      return;
+    }
+    let ctx = cnv.getContext("2d");
 
-  // Global Variables
-  let mouseIsPressed = false;
-  let mouseX, mouseY, pmouseX, pmouseY;
-  let penSize = 55; // Porportionate to canvas size for MNIST Dataset
-  let penColor = "black"; // Default pen color
-  let eraserSize = 75; 
-  let eraserColor = "white"; // Default eraser color
+    // Global Variables
+    let mouseIsPressed = false;
+    let mouseX, mouseY, pmouseX, pmouseY;
+    let penSize = 55; // Proportionate to canvas size for MNIST Dataset
+    let penColor = "black"; // Default pen color
 
-  
+    // Main Program Loop (60 FPS)
+    requestAnimationFrame(loop);
 
+    function loop() {
+      // Draw a circle if mouseIsPressed
+      if (mouseIsPressed) {
+        ctx.strokeStyle = penColor;
+        ctx.lineWidth = penSize;
+        ctx.beginPath();
+        ctx.moveTo(pmouseX, pmouseY);
+        ctx.lineTo(mouseX, mouseY);
+        ctx.stroke();
+      }
+      requestAnimationFrame(loop);
+    }
 
+    // Mouse Events
+    document.addEventListener("mousedown", mousedownHandler);
+    document.addEventListener("mouseup", mouseupHandler);
+    document.addEventListener("mousemove", mousemoveHandler);
 
+    function mousedownHandler() {
+      mouseIsPressed = true;
+    }
+
+    function mouseupHandler() {
+      mouseIsPressed = false;
+    }
+
+    function mousemoveHandler(event) {
+      // Save previous mouseX and mouseY
+      pmouseX = mouseX;
+      pmouseY = mouseY;
+
+      // Update mouseX and mouseY
+      let cnvRect = cnv.getBoundingClientRect();
+      mouseX = event.clientX - cnvRect.left;
+      mouseY = event.clientY - cnvRect.top;
+    }
+  },
+};
 </script>
+
 
 <style scoped>
 #canvas-container {
