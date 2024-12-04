6c968d9c320c145838ec61302f7171bcb5db736f
Tioluwani <43251329+t1oluwani@users.noreply.github.com>
2024-12-04 12:07:56 -0700

    fill out api urls

--- frontend/src/service/MLModelClient.js
+++ frontend/src/service/MLModelClient.js
@@ -3,17 +3,17 @@ import axios from "axios";
 const API_URL = "http://localhost:5000"; // Backend URL
 
 async function trainModel(model_type) {
-  const response = await axios.post(`${API_URL}/...`);
+  const response = await axios.post(`${API_URL}/train/${model_type}`);
   return response.data;
 }
 
 async function clearModel(model_type) {
-  const response = await axios.post(`${API_URL}/...`);
+  const response = await axios.post(`${API_URL}/clear/${model_type}`);
   return response.data;
 }
 
 async function modelPredict(model_type, img_data) {
-  const response = await axios.post(`${API_URL}/...`, img_data);
+  const response = await axios.post(`${API_URL}/predict/${model_type}`, img_data);
   return response.data;
 }
 
